07fc200b35835fae15eda2af2990ef210d948df8
GuilhermeMM <114604987+Guilherme-Medeiros-de-Melo@users.noreply.github.com>
2025-08-02 16:16:19 -0300

    (WIP) Added investmentData processing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { ResultTableComponent } from './result-table/result-table.component';
+import {
+  InvestimentData,
+  YearlyInvestmentReturns,
+} from './user-input/user-input.model';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +13,64 @@ import { ResultTableComponent } from './result-table/result-table.component';
   imports: [HeaderComponent, UserInputComponent, ResultTableComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  years!: number;
+  months!: number;
+  grossValue!: number;
+  netValue!: number;
+  yearlyInvestment!: number;
+  taxRate!: number;
+  totalTax!: number;
+  yearlyReturnRate!: number;
+
+  investmentReturns!: YearlyInvestmentReturns[];
+
+  onCalculateInvestmentResults(investmentData: InvestimentData) {
+    this.essentialsLog();
+    
+    this.taxRate = this.calculateTaxRate(investmentData.tempo);
+
+    this.yearlyReturnRate = investmentData.selic * (investmentData.rendimentoCdi / 100);
+    this.grossValue = investmentData.valorInicial;
+    this.yearlyInvestment = investmentData.aplicacaoAnual;
+    this.years = Math.floor(investmentData.tempo / 12);
+    this.months = investmentData.tempo % 12;
+
+    const appliedValue = this.grossValue + this.yearlyInvestment;
+    const grossValue = appliedValue * this.yearlyReturnRate;
+    const taxValue = appliedValue * this.taxRate;
+    const netValue = grossValue - taxValue;
+    const netValuePercentage = (netValue - appliedValue) / appliedValue;
+
+    for (let year = 0; year <= this.years; year++) {
+      this.investmentReturns.push({
+        appliedValue: appliedValue,
+        grossValue: grossValue,
+        taxValue: taxValue,
+        netValue: netValue,
+        netValuePercentage: netValuePercentage,
+      });
+    }
+  }
+
+  calculateTaxRate(tempo: number) {
+    if (tempo <= 6) {
+      return 0.225;
+    } else if (tempo <= 12) {
+      return 0.2;
+    } else if (tempo <= 24) {
+      return 0.175;
+    } else {
+      return 0.15;
+    }
+  }
+
+  essentialsLog(){
+    console.log(this.years);
+    console.log(this.months);
+    console.log(this.taxRate);
+    console.log(this.yearlyInvestment);
+    console.log(this.yearlyReturnRate);
+    console.log(this.grossValue);
+  }
+}
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { ResultTableComponent } from '../result-table/result-table.component';
+import { FormsModule } from '@angular/forms';
+import { InvestimentData } from './user-input.model';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ResultTableComponent],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  valorInicial!: number;
+  aplicacaoAnual!: number;
+  tempo!: number;
+  selic!: number;
+  rendimentoCdi!: number;
 
+  calculate = output<InvestimentData>();
+
+  onSubmit() {
+    this.calculate.emit({
+      valorInicial: this.valorInicial,
+      aplicacaoAnual: this.aplicacaoAnual,
+      tempo: this.tempo,
+      selic: this.selic,
+      rendimentoCdi: this.rendimentoCdi,
+    });
+  }
 }
