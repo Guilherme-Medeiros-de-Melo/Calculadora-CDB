e7eae25630d736609b2ac3c6e007f461050e4aa0
GuilhermeMM <114604987+Guilherme-Medeiros-de-Melo@users.noreply.github.com>
2025-08-02 16:14:36 -0300

    Added investment models and form event

---
+++ src/app/user-input/user-input.model.ts
@@ -0,0 +1,15 @@
+export class InvestimentData {
+  valorInicial!: number;
+  aplicacaoAnual!: number;
+  tempo!: number;
+  selic!: number;
+  rendimentoCdi!: number;
+}
+
+export class YearlyInvestmentReturns {
+    appliedValue!:number;
+    grossValue!:number;
+    taxValue!:number;
+    netValue!:number;
+    netValuePercentage!:number;
+}
